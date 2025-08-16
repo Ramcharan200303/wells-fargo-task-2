3167f22182a1d75e6f6c83a6bc83298e4767dc7a
Ramcharan21 <105923873+Ramcharan200303@users.noreply.github.com>
2025-08-17 00:24:25 +0530

    Update Portfolio.java

--- src/main/java/com/wellsfargo/counselor/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/Portfolio.java
@@ -1,22 +1,41 @@
-package com.wellsfargo.counselor;
+package com.wellsfargo.entity;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
 public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    private String portfolioName;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
     private Client client;
+
+    @OneToMany(mappedBy = "portfolio")
     private List<Security> securities;
 
     // Constructor
-    public Portfolio(int id, Client client, List<Security> securities) {
-        this.id = id;
+    public Portfolio(String portfolioName, Client client) {
+        this.portfolioName = portfolioName;
         this.client = client;
-        this.securities = securities;
+    }
+
+    // Default constructor (needed for JPA)
+    public Portfolio() {
     }
 
     // Getters
     public int getId() {
         return id;
+    }
+
+    public String getPortfolioName() {
+        return portfolioName;
     }
 
     public Client getClient() {
@@ -28,11 +47,24 @@ public class Portfolio {
     }
 
     // Setters
+    public void setPortfolioName(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
     public void setClient(Client client) {
         this.client = client;
     }
 
     public void setSecurities(List<Security> securities) {
         this.securities = securities;
+    }
+
+    @Override
+    public String toString() {
+        return "Portfolio{" +
+                "id=" + id +
+                ", portfolioName='" + portfolioName + '\'' +
+                ", client=" + client +
+                '}';
     }
 }
