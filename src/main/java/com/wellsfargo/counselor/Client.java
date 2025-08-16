6ca84c75178f9c96e02911b26b9fc3bfa40497f6
Ramcharan21 <105923873+Ramcharan200303@users.noreply.github.com>
2025-08-17 00:01:42 +0530

    Create Client.java

---
+++ src/main/java/com/wellsfargo/counselor/Client.java
@@ -0,0 +1,36 @@
+package com.wellsfargo.counselor;
+
+public class Client {
+    private int id;
+    private String name;
+    private String email;
+
+    // Constructor
+    public Client(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
