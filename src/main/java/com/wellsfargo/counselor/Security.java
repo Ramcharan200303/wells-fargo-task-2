06e6a11babf4206e6fb648045a56ed07ff01b74a
Ramcharan21 <105923873+Ramcharan200303@users.noreply.github.com>
2025-08-17 00:05:32 +0530

    Create Security.java

---
+++ src/main/java/com/wellsfargo/counselor/Security.java
@@ -0,0 +1,46 @@
+package com.wellsfargo.counselor;
+
+public class Security {
+    private int id;
+    private String name;
+    private String type;
+    private double price;
+
+    // Constructor
+    public Security(int id, String name, String type, double price) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
